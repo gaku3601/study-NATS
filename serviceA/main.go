62b01eaaaea19408f876d6b8ce8c79ec969d819e
gaku3601 <pro.gaku@gmail.com>
2018-04-29 10:13:18 +0900

    いろいろ検証中

--- serviceA/main.go
+++ serviceA/main.go
@@ -2,23 +2,52 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"time"
 
+	"github.com/labstack/echo"
 	"github.com/nats-io/nats"
 )
 
 func main() {
+	// Echo instance
+	e := echo.New()
+
+	// Routes
+	e.GET("/", index)
+	e.GET("/simplicity", simplicity)
+
+	// Start server
+	e.Logger.Fatal(e.Start(":8080"))
+
+}
+
+// Handler
+func index(c echo.Context) error {
 	// Connect to server; defer close
-	natsConnection, _ := nats.Connect(nats.DefaultURL)
+	natsConnection, err := nats.Connect(nats.DefaultURL)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, "Nats Server: "+err.Error())
+	}
 	defer natsConnection.Close()
 	log.Println("Connected to " + nats.DefaultURL)
+
 	// Publish message on subject
 	subject := "foo"
 	natsConnection.Publish(subject, []byte("Hello NATS"))
-	log.Println("Published message on subject " + subject)
 
 	// Simple Sync Subscriber
 	sub, _ := natsConnection.SubscribeSync("resfoo")
-	m, _ := sub.NextMsg(10 * time.Millisecond)
-	log.Printf("Received message '%s\n", string(m.Data)+"'")
+	m, err := sub.NextMsg(10 * time.Millisecond)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, "ServiceB Connection: "+err.Error())
+	}
+	return c.String(http.StatusOK, string(m.Data))
+}
+
+func simplicity(c echo.Context) error {
+	nc, _ := nats.Connect(nats.DefaultURL)
+	// Requests
+	msg, _ := nc.Request("simplicity", []byte("help me"), 10*time.Millisecond)
+	return c.String(http.StatusOK, string(msg.Data))
 }
--- serviceB/main.go
+++ serviceB/main.go
@@ -2,8 +2,8 @@ package main
 
 // Import Go and NATS packages
 import (
+	"fmt"
 	"log"
-	"runtime"
 
 	"github.com/nats-io/nats"
 )
@@ -14,13 +14,31 @@ func main() {
 	log.Println("Connected to " + nats.DefaultURL)
 	// Subscribe to subject
 	log.Printf("Subscribing to subject 'foo'\n")
-	natsConnection.Subscribe("foo", func(msg *nats.Msg) {
-		// Handle the message
-		log.Printf("Received message '%s\n", string(msg.Data)+"'")
+	// Channel Subscriber
+	errChan := make(chan error)
+	go func() {
+		_, err := natsConnection.Subscribe("foo", func(msg *nats.Msg) {
+			// Handle the message
+			log.Printf("Received message '%s\n", string(msg.Data)+"'")
 
-		//返信
-		natsConnection.Publish("resfoo", []byte("response message!!!!!!!!!!!!!!"))
-	})
-	// Keep the connection alive
-	runtime.Goexit()
+			//返信
+			natsConnection.Publish("resfoo", []byte("response message!!!!!!!!!!!!!!"))
+		})
+		if err != nil {
+			errChan <- err
+		}
+	}()
+	// simplicity
+	go func() {
+		// Replies
+		_, err := natsConnection.Subscribe("simplicity", func(m *nats.Msg) {
+			// Handle the message
+			log.Printf("Received message '%s\n", string(m.Data)+"'")
+			natsConnection.Publish(m.Reply, []byte("I can help!"))
+		})
+		if err != nil {
+			errChan <- err
+		}
+	}()
+	fmt.Println(<-errChan)
 }
